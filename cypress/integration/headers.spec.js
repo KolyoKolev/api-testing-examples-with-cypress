65b8f20b7ec0a4ca4278264172394370645e80d1
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-15 23:10:50 +0200

    add init tests for the headers spec

---
+++ cypress/integration/headers.spec.js
@@ -0,0 +1,17 @@
+import ENDPOINTS from '../constants/endPoints';
+
+Cypress.Commands.add('getEndpointHeader', (header) => {
+  cy.get('@usersEndpoint').its('headers').its(header);
+});
+
+describe('Headers', () => {
+  beforeEach('should request the endpoint under test', () => {
+    cy.request(ENDPOINTS.user).as('usersEndpoint');
+  });
+
+  it('should assert the values of the page content type header', () => {
+    cy.getEndpointHeader('content-type')
+      .should('include', 'application/json')
+      .and('include', ' charset=utf-8');
+  });
+});
