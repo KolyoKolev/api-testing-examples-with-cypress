b2284dad914874e6c4242328948673e5e9730d5e
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-16 19:56:52 +0200

    add test for creating new user

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -1,5 +1,14 @@
 import ENDPOINTS from '../constants/endpoints';
 
+const RANDOM_ID = Cypress._.random(0, 100);
+
+const NEW_USER = {
+  id: RANDOM_ID,
+  email: 'dummy_mail@dummy.dummy',
+  first_name: 'Dummy',
+  last_name: 'Name',
+};
+
 Cypress.Commands.add('requestUsersEndpoint', () => {
   cy.request(ENDPOINTS.users);
 });
@@ -8,14 +17,30 @@ Cypress.Commands.add('getUsersResponseBodyData', () => {
   cy.requestUsersEndpoint().its('body').its('data');
 });
 
-describe('Users', () => {
-  it('should assert the status code', () => {
-    cy.requestUsersEndpoint().its('status').should('eq', 200);
+describe('Users API', () => {
+  context('GET requests', () => {
+    it('should assert the status code', () => {
+      cy.requestUsersEndpoint().its('status').should('eq', 200);
+    });
+
+    it('should assert the number of users', () => {
+      cy.getUsersResponseBodyData()
+        .should('be.an', 'array')
+        .and('have.length', 6);
+    });
   });
 
-  it('should assert the number of users', () => {
-    cy.getUsersResponseBodyData()
-      .should('be.an', 'array')
-      .and('have.length', 6);
+  context('POST requests', () => {
+    it('should create a new user', () => {
+      cy.request('POST', ENDPOINTS.users, NEW_USER).then((res) => {
+        expect(res.status).to.eq(201);
+        expect(Object.keys(res.body)).to.have.length(5);
+        expect(res.body.id).to.eq(NEW_USER.id);
+        expect(res.body.email).to.eq(NEW_USER.email);
+        expect(res.body.first_name).to.eq(NEW_USER.first_name);
+        expect(res.body.last_name).to.eq(NEW_USER.last_name);
+        expect(res.body.not_existing_key_pair).to.eq(undefined);
+      });
+    });
   });
 });
