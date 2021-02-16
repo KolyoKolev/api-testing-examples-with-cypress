ab780b240ba9245eb6a9c5c678a9f5fa58582c31
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-16 17:46:00 +0200

    add custom command for requesting body data

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -4,6 +4,10 @@ Cypress.Commands.add('requestUsersEndpoint', () => {
   cy.request(ENDPOINTS.users);
 });
 
+Cypress.Commands.add('getUsersResponseBodyData', () => {
+  cy.requestUsersEndpoint().its('body').its('data');
+});
+
 describe('Users', () => {
   it('should assert the status code', () => {
     cy.requestUsersEndpoint().its('status').should('eq', 200);
