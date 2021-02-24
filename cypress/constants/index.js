adfc96a182a9f20edbd55ba28db174403558a5e7
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-24 09:03:48 +0200

    refactor the test data

--- cypress/constants/index.js
+++ cypress/constants/index.js
@@ -1,8 +1,13 @@
 const RANDOM_ID = Cypress._.random(0, 100);
+const id = RANDOM_ID;
+const email = 'dummy_mail@dummy.dummy';
+const password = 'dummy_password';
+const firstName = 'Dummy';
+const lastName = 'Name';
 
 export const NEW_USER = {
-  id: RANDOM_ID,
-  email: 'dummy_mail@dummy.dummy',
-  first_name: 'Dummy',
-  last_name: 'Name',
+  id,
+  email,
+  firstName,
+  lastName,
 };
